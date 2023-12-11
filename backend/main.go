778ddd7bfe64429f857182394a11a1106b3f5a42
javierlinero <jl5951@princeton.edu>
2023-12-10 21:42:27 -0500

    send it

--- backend/main.go
+++ backend/main.go
@@ -82,6 +82,7 @@ var Sessions SessionMap
 
 func CreateSessionRequestHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json")
 	sessionID := Sessions.MakeSession()
 
 	log.Println("created session: ", sessionID)
