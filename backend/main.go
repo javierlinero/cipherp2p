a86014df1972acca0a6e90cd5c0599400852d24e
javierlinero <jl5951@princeton.edu>
2023-12-13 19:21:49 -0500

    debugging somethng

--- backend/main.go
+++ backend/main.go
@@ -353,6 +353,7 @@ func GetAPIKeyHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Content-Type", "application/json")
 	apiKey := os.Getenv("TURN_API_KEY")
+	print(apiKey)
 	response := map[string]string{"apiKey": apiKey}
 	json.NewEncoder(w).Encode(response)
 }
